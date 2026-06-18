9a910baaf1b728ea0e6e42d8c978e5d12ac1d69b
josephuk77 <josephuk77@gmail.com>
2026-06-18 09:11:14 +0900

    fix: #28 FCM no-op port 조건부 등록

--- src/main/java/com/faithlog/user/infrastructure/fcm/NoOpCurrentDeviceFcmTokenDeactivationAdapter.java
+++ src/main/java/com/faithlog/user/infrastructure/fcm/NoOpCurrentDeviceFcmTokenDeactivationAdapter.java
@@ -2,9 +2,7 @@ package com.faithlog.user.infrastructure.fcm;
 
 import com.faithlog.user.application.port.CurrentDeviceFcmTokenDeactivationCommand;
 import com.faithlog.user.application.port.CurrentDeviceFcmTokenDeactivationPort;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NoOpCurrentDeviceFcmTokenDeactivationAdapter implements CurrentDeviceFcmTokenDeactivationPort {
 
 	@Override
